48206e541652ab88ca183d4ff9bba57cbc4dffef
SebastianVP-Seb <palacios.sebastian.24@outlook.com>
2021-11-05 20:44:06 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const divSelector=document.querySelectorAll('.select');
+
+// divSelector.addEventListener(onclick(e)=>(console.log('click')))
+
+const voltearImg=(e)=>{
+    console.log(e.target.value);
+};
